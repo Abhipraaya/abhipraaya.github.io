65164336443e6038407d13753119761560c6d757
Abhipraaya <155866449+Abhipraaya@users.noreply.github.com>
2026-09-13 13:05:44 +0545

    deleting old files

--- script.js
+++
@@ -1,7 +0,0 @@
-const hamburger = document.getElementById('hamburger');
-const navLink = document.getElementById('navLink')
-
-hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle('active');
-    navLink.classList.toggle('active')
-})
